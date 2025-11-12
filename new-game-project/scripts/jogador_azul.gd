0cd1e43d7a1198e462215062f617dd7caed162dc
godwers <gondes.historia@gmail.com>
2025-11-12 16:31:08 -0300

    atualizando

--- new-game-project/scripts/jogador_azul.gd
+++ new-game-project/scripts/jogador_azul.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-@onready var forca = 500
-@onready var hp = 10
+@onready var forca = 500 
+@onready var hp = 5
 
-func _physics_process(delta : float) -> void:
+func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("jogador_azul_esquerda","jogador_azul_direita","jogador_azul_cima","jogador_azul_baixo") * forca 
+
+	if Input.is_action_pressed("jogador_azul_atirar"):
+		print_debug("jogador azul atirou")
 	move_and_slide()
--- new-game-project/scripts/jogador_vermelho.gd
+++ new-game-project/scripts/jogador_vermelho.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 @onready var forca = 500 
-@onready var hp = 10
+@onready var hp = 5
 
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("jogador_vermelho_esquerda","jogador_vermelho_direita","jogador_vermelho_cima","jogador_vermelho_baixo") * forca
+
+	if Input.is_action_pressed("jogador_vermelho_atirar"):
+		print_debug("jogador vermelho atirou")
 	move_and_slide()
